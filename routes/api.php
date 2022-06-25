b6f5ccc4cfb7de35e6abc428e70956271e666f2a
mahdi <mahdi@kali>
2022-06-25 19:31:02 +0430

    Added REST Api

---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/me', [AuthController::class, 'me'])->middleware('auth:sanctum');
+
+Route::prefix(v1)->group(function(){
+
+    Route::middleware('auth:sanctum')->group(function (){
+
+    });
+});
+
+
